4cfd39f5a0eaea1301fbe468a61fbdf9065a83e7
yohcard <cardisyohan@gmail.com>
2024-06-05 11:25:40 +0200

    update

--- app/src/App.vue
+++ app/src/App.vue
@@ -2,26 +2,24 @@
 import { RouterLink, RouterView } from 'vue-router'
 import homeView from './views/HomeView.vue'
 import TheHeader from './components/TheHeader.vue'
-
+import TheFooter from './components/TheFooter.vue'
 </script>
 
 <template>
-    <div class="mainPage">
+  <div class="mainPage">
     <header>
-        <TheHeader/>
+      <TheHeader/>
     </header>
     <div class="main">
-        <div>
-            <homeView/>
-        </div>
-        
-       
-            <RouterView/>
-        
+      <div>
+        <homeView/>
+      </div>
+      <RouterView/>
     </div>
-</div>
-    
-
+    <footer>
+      <TheFooter/>
+    </footer>
+  </div>
 </template>
 
 <style scoped>
@@ -34,17 +32,21 @@ header {
   justify-content: left;
   background-color: #333; 
 }
-.main{
-    width: 200%;
-    display: grid;
-    grid-template-columns: 50% 50%;
-    grid-template-rows: 50%;
+
+.main {
+  width: 200%;
+  display: grid;
+  grid-template-columns: 50% 50%;
+  grid-template-rows: 50%;
 }
-.divRouterView{
-    padding: 0%;
-    margin: 0%;
+
+.divRouterView {
+  padding: 0%;
+  margin: 0%;
 }
-.mainPage{
-    width: 100%;
+
+.mainPage {
+  width: 100%;
+  height: 80%;
 }
 </style>
---
+++ app/src/components/TheFooter.vue
@@ -0,0 +1,8 @@
+<template>
+<p class="TheFooter">Site des innoWeeks créer par TAJYD</p>
+</template>
+<style scoped>
+.TheFooter{
+    text-align: center;
+}
+</style>
