311efe7b18199b3572367f1838af38e6bda7cbe7
darchasi <pv20qck@eduvaud.ch>
2024-06-24 14:31:56 +0200

    Correction des exercices

--- app/src/views/ExerciceView3.vue
+++ app/src/views/ExerciceView3.vue
@@ -1,25 +1,28 @@
 <template>
-  <div class="contact-form">
-    <h1>
-      Exercices <br />
-      Envois d'un mail
-    </h1>
-    <form @submit.prevent="submitForm">
-      <div>
-        <label for="email">Email du destinataire :</label>
-        <input type="email" id="email" v-model="form.email" required />
-      </div>
-      <div>
-        <label for="text">Objet de votre mail :</label>
-        <input type="text" id="text" v-model="form.text" required />
-      </div>
-      <div>
-        <label for="message">Message :</label>
-        <textarea id="message" v-model="form.message" required></textarea>
-      </div>
-      <button type="submit">Envoyer</button>
-    </form>
+  <div v-if="isLoginFormVisible" id="loginForm">
+    <div class="contact-form">
+      <h1>
+        Exercices <br />
+        Envois d'un mail
+      </h1>
+      <form @submit.prevent="submitForm">
+        <div>
+          <label for="email">Email du destinataire :</label>
+          <input type="email" id="email" v-model="form.email" required />
+        </div>
+        <div>
+          <label for="text">Objet de votre mail :</label>
+          <input type="text" id="text" v-model="form.text" required />
+        </div>
+        <div>
+          <label for="message">Message :</label>
+          <textarea id="message" v-model="form.message" required></textarea>
+        </div>
+        <button type="submit">Envoyer</button>
+      </form>
+    </div>
   </div>
+  <div v-else><button @click="toggleLoginForm">Commencer</button></div>
 </template>
 
 <script>
@@ -35,24 +38,33 @@ export default {
     })
 
     const router = useRouter()
+    const isLoginFormVisible = ref(false)
+
+    const toggleLoginForm = () => {
+      alert(
+        'Le premier exercice consiste à se connecter à une page de login.\nVoici les données à insérer:\nEmail du destinataire : : distanataire@example.com\nObjet de votre mail : Objet\nMessage : Ceci est un message.'
+      )
+      isLoginFormVisible.value = !isLoginFormVisible.value
+      console.log('isLoginFormVisible:', isLoginFormVisible.value)
+    }
 
     const submitForm = () => {
       const expectedEmail = 'distanataire@example.com'
-      const expectedText = 'objet execices deux'
-      const expectedMessage = 'Ceci est un message de d exemple pour l exercices deux.'
+      const expectedText = 'Objet'
+      const expectedMessage = 'Ceci est un message.'
 
       if (
         form.value.email === expectedEmail &&
         form.value.text === expectedText &&
         form.value.message === expectedMessage
       ) {
-        alert('Vous avez reussit l exercice 2 bien jouer !')
+        alert('Vous avez reussit l exercice 3 bien jouer !')
 
-        router.push('/exercices3')
+        router.push('/exercices')
       } else {
         if (form.value.email !== expectedEmail) {
           alert(
-            "L'email du destinataire est incorrect.\n\nConseil : Assurez-vous que l'email correspond à test@example.com."
+            "L'email du destinataire est incorrect.\n\nConseil : Assurez-vous que l'email correspond à distanataire@example.com."
           )
         } else if (form.value.text !== expectedText) {
           alert(
@@ -68,6 +80,8 @@ export default {
 
     return {
       form,
+      isLoginFormVisible,
+      toggleLoginForm,
       submitForm
     }
   }
